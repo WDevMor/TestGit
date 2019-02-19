9458a438fe445c0208af34bdb6625ce9a491e365
devinmorris <devinmorris@192.168.0.117>
2019-02-19 05:13:40 -0800

    First Commit

---
+++ mjava/src/javatry.java
@@ -0,0 +1,9 @@
+
+public class javatry {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ mjava/src/mjava/Javatutorial.java
@@ -0,0 +1,12 @@
+package mjava;
+
+public class Javatutorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World. What Can I do now?");
+		
+		
+	}
+
+}
